4f967aa3bf1b69181baec310dccc20011767ad67
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-06-24 21:10:12 +0900

    Utilityクラス（基本的な情報の格納先）の作成

--- EvolutionaryComputing.java
+++ EvolutionaryComputing.java
@@ -3,7 +3,12 @@
  */
 public class EvolutionaryComputing {
     public static void main(String[] args) throws Exception{
-
+        Utility.setMAX_FUNC_EVAL(200000);
+        System.out.println("FEs=" + Utility.getMAX_FUNC_EVAL());
+        Utility.setDIMENSION(30);
+        System.out.println("DIM=" + Utility.getDIMENSION());
+        Utility.setRUN_MAX(30);
+        System.out.println("RUN_MAX=" + Utility.getRUN_MAX());
 
     }
 }
---
+++ Utility.java
@@ -0,0 +1,34 @@
+/**
+ * Created by toshiki on 2017/06/24.
+ */
+public class Utility {
+    /* Constructor */
+    private Utility(){}
+
+    /* Member */
+    private static int MAX_FUNC_EVAL;
+    private static int DIMENSION;
+    private static int RUN_MAX;
+
+    /* Method */
+    public static void setMAX_FUNC_EVAL(int MAX_FUNC_EVAL){
+        Utility.MAX_FUNC_EVAL = MAX_FUNC_EVAL;
+    }
+    public static int getMAX_FUNC_EVAL(){
+        return MAX_FUNC_EVAL;
+    }
+    public static void setDIMENSION(int DIMENSION) {
+        Utility.DIMENSION = DIMENSION;
+    }
+    public static int getDIMENSION() {
+        return DIMENSION;
+    }
+    public static int getRUN_MAX() {
+        return RUN_MAX;
+    }
+    public static void setRUN_MAX(int RUN_MAX) {
+        Utility.RUN_MAX = RUN_MAX;
+    }
+
+}
+
