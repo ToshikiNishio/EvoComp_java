f5f189b780390a4e65ac5245ec65a5c6afcdc0ef
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-06-24 21:24:40 +0900

    ProblemUtilクラスの定義

---
+++ ProblemUtil.java
@@ -0,0 +1,13 @@
+/**
+ * Created by toshiki on 2017/06/24.
+ */
+public class ProblemUtil {
+    /* Constructor */
+    private ProblemUtil() {}
+
+    /* Member */
+    private enum Basic_ProblemID {
+        Sphere, Shwefel_P2_22, Quadric, Rosenbrock, Step, Quadric_Noise,
+        Schwefel, Rastrigin, Noncontinuous_Rastrigin, Ackley, Griewank, Generalized_Penalized
+    };
+}
