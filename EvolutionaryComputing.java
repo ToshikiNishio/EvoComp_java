a9190255ef505b5220539be10f5e101d9f5e288e
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-06-28 23:11:13 +0900

    ・列挙子CEC2015の定義 ・関数IDにより関数のタイプが何かを判断するための変数を定義

--- EvolutionaryComputing.java
+++ EvolutionaryComputing.java
@@ -17,7 +17,8 @@ public class EvolutionaryComputing {
         ind1.positon[3] = 5;
         System.out.println(ProblemUtil.Basic_ProblemID.Sphere.calc_fit(ind1.positon));
 
-        ProblemUtil.InitBounds(ProblemUtil.Basic_ProblemID.Sphere);
+        ProblemUtil.setProblemID(ProblemUtil.Basic_ProblemID.Sphere);
+        ProblemUtil.setProblemID(ProblemUtil.CEC2015_ProblemID.tmp1);
         System.out.println("Lower = " + ProblemUtil.getProg_Lower());
         System.out.println("Upper =" + ProblemUtil.getProg_Upper());
     }
