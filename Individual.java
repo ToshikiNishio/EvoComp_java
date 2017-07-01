f466463c2ffa541e0d2128dbe276045a27f28db2
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-07-02 00:43:47 +0900

    Individualクラスで個体の表示するメソッドの分割

--- Individual.java
+++ Individual.java
@@ -16,10 +16,18 @@ public class Individual {
         current_fitness = ProblemUtil.calcFitness(positon);
     }
 
+    /* Print information of individual */
     public void printIndivisual(){
+        printCurrentPosition();
+        printCurrentFitness();
+    }
+    public void printCurrentPosition() {
         for (int i = 0; i<Utility.getDIMENSION(); i++) {
             System.out.println("position[" + i + "] = " + positon[i]);
         }
+    }
+    public void printCurrentFitness() {
         System.out.println("current_fitness = " + current_fitness);
     }
+
 }
