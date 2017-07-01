3699f4370ee04e0acb3eec5607c1c4f1f3009b25
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-07-01 20:28:05 +0900

    Utilityクラスのリファクタリング

--- Utility.java
+++ Utility.java
@@ -11,29 +11,31 @@ public class Utility {
     private static int RUN_MAX;
     public static Sfmt Rnd;
 
-    /* Method */
+    /* Set Parameter */
     public static void setMAX_FUNC_EVAL(int MAX_FUNC_EVAL){
         Utility.MAX_FUNC_EVAL = MAX_FUNC_EVAL;
-    }
-    public static int getMAX_FUNC_EVAL(){
-        return MAX_FUNC_EVAL;
     }
     public static void setDIMENSION(int DIMENSION) {
         Utility.DIMENSION = DIMENSION;
     }
-    public static int getDIMENSION() {
-        return DIMENSION;
+    public static void setRUN_MAX(int RUN_MAX) {
+        Utility.RUN_MAX = RUN_MAX;
+    }
+
+    /*Get Parameter*/
+    public static int getDIMENSION() {return DIMENSION;}
+    public static int getMAX_FUNC_EVAL(){
+        return MAX_FUNC_EVAL;
     }
     public static int getRUN_MAX() {
         return RUN_MAX;
     }
-    public static void setRUN_MAX(int RUN_MAX) {
-        Utility.RUN_MAX = RUN_MAX;
-    }
+
+    /* SIMD-oriented Fast Mersenne Twister */
     public static void setRnd(int seed){Utility.Rnd = new Sfmt(seed);}
-
-
     public static double rand(){return Rnd.NextUnif();}
+
+    /* Utility method */
     public static double square(double x){return x * x;}
 
 }
