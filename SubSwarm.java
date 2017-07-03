1a02b69831f210de7a0668e95c8166f5381a448d
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-07-04 00:50:07 +0900

    速度を更新するメソッドの追加

--- SubSwarm.java
+++ SubSwarm.java
@@ -28,9 +28,20 @@ public class SubSwarm {
             Lbest_fitness = par.hist_best_fit;
         }
     }
+    public void printLbest(){System.out.println("Lbest= " + Lbest_index);}
+    /* Update particles */
+    public void updateVelocity(){
+        double c1 = PSOUtil.getC1();
+        double c2 = PSOUtil.getC2();
+        double w  = PSOUtil.currentIW;
+        int    G  = Lbest_index;
 
-    public void printLbest(){
-        System.out.println("Lbest= " + Lbest_index);
+        for (Particle par : particles) {
+            for (int dim = 0; dim < Utility.getDIMENSION(); dim++) {
+                par.velocity[dim] = w * par.velocity[dim]
+                                    + c1 * Utility.rand() * (par.hist_best_pos[dim] - par.positon[dim])
+                                    + c2 * Utility.rand() * (particles.get(G).hist_best_pos[dim] - par.positon[dim]);
+            }
+        }
     }
-
 }
