24475c2f7957b2b642d593dd08db9e4dc42afd81
nishiotoshiki <toshiki@nishiotoshiki-no-MacBook-Air.local>
2017-07-03 23:58:53 +0900

    PSOUtil（PSO固有のパメメータの設定場所）クラスの定義

---
+++ PSOUtil.java
@@ -0,0 +1,17 @@
+/**
+ * Created by toshiki on 2017/07/03.
+ */
+public class PSOUtil {
+    /* Constructor */
+    private PSOUtil() {}
+
+    /* Member */
+    private static double MAX_MAGNITUDE_VELOCITY;
+
+    /* Getter */
+    public static double getMAX_MAGNITUDE_VELOCITY() {
+        return MAX_MAGNITUDE_VELOCITY;
+    }
+
+
+}
