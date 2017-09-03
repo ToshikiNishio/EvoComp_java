a528c6d10d62f9e8cb627722106d556d25cf082f
nishio <toshiki@nishiotoshiterunoMac-Pro.local>
2017-09-03 17:07:09 +0900

    データをcsvに書き込むメソッドの定義

--- GPSO.java
+++ GPSO.java
@@ -8,18 +8,21 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class GPSO implements Algorithm {
+    /* Parameter Settings */
+    double MAX_IW = 0.9;   //Maximum inertia weight
+    double MIN_IW = 0.4;   //Minimum inertia weight
+    double C1 = 2.0;       //Acceleration Coefficients 1
+    double C2 = 2.0;       //Acceleration Coefficients 2
+    int    SUB_SWARM_SIZE = 20;
+    /* Variable definition */
+    String folderName;
+    double currentIW;
     public void run(int run){
-        double MAX_IW = 0.9;   //Maximum inertia weight
-        double MIN_IW = 0.4;   //Minimum inertia weight
-        double C1 = 2.0;       //Acceleration Coefficients 1
-        double C2 = 2.0;       //Acceleration Coefficients 2
-        int    SUB_SWARM_SIZE = 20;
-
         Utility.cur_func_eval = 0;
         Utility.cur_generation = 0;
 
         SubSwarm sub1 = new SubSwarm(SUB_SWARM_SIZE);
-        double currentIW = MAX_IW;
+        currentIW = MAX_IW;
 
         makeFiles(run);
 
@@ -32,16 +35,17 @@ public class GPSO implements Algorithm {
                         * Utility.cur_func_eval / Utility.getMAX_FUNC_EVAL();
 
             Utility.cur_generation++;
+            writeFile(sub1);
         }
         /* Input best particle for Output */
         Utility.stack_hist_best_fit[run] = sub1.getLbest_fitness();
         //Utility.printFinalBest(run, sub1);
     }
 
-    public void makeFiles(int run){
+    private void makeFiles(int run){
         /* Make Folder */
         String className = new Object(){}.getClass().getEnclosingClass().getName();
-        String folderName = "/Users/toshiki/Output/" + Utility.date + "/" + className +
+        folderName = "/Users/toshiki/Output/" + Utility.date + "/" + className +
                                     "/" + ProblemUtil.getProb_obj() + "/RUN" + run;
         File newfile = new File(folderName);
         newfile.mkdirs();
@@ -62,6 +66,30 @@ public class GPSO implements Algorithm {
             pw.print("c1");
             pw.print(",");
             pw.print("c2");
+            pw.println();
+            //Close file
+            pw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void writeFile(SubSwarm swarm){
+        try {
+            FileWriter fw = new FileWriter(folderName + "/output.csv", true); //追記モード
+            //Write header
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            pw.print(Utility.cur_generation);
+            pw.print(",");
+            pw.print(Utility.cur_func_eval);
+            pw.print(",");
+            pw.print(swarm.getLbest_fitness());
+            pw.print(",");
+            pw.print(currentIW);
+            pw.print(",");
+            pw.print(C1);
+            pw.print(",");
+            pw.print(C2);
             pw.println();
             //Close file
             pw.close();
