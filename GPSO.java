6556c545a84ffaef75ded29b0c37fa95ac09f38a
nishio <toshiki@nishiotoshiterunoMac-Pro.local>
2017-09-18 18:12:55 +0900

    散布図出力するためにcsvに位置を出力するメソッドを作成。 問題点: シャローコピー、pbestの位置、clear()の別関数化

--- GPSO.java
+++ GPSO.java
@@ -24,6 +24,8 @@ public class GPSO implements Algorithm {
     public ArrayList<Integer> recordEvalTimes =new ArrayList<Integer>();
     public ArrayList<Double> recordInertiaWeight =new ArrayList<Double>();
     public ArrayList<Double> recordBestFitness =new ArrayList<Double>();
+    //public ArrayList<ArrayList<Double[]>> recordPosition = new ArrayList();
+    public ArrayList<SubSwarm> recordPosition = new ArrayList<>();
 
     public void run(int run){
         Utility.cur_func_eval = 0;
@@ -33,6 +35,7 @@ public class GPSO implements Algorithm {
         currentIW = MAX_IW;
 
         makeParameterFiles(run);
+        makeScatterFiles(run);
 
         while (Utility.cur_func_eval < Utility.getMAX_FUNC_EVAL()) {
             sub1.updateVelocity(currentIW, C1, C2);
@@ -45,12 +48,14 @@ public class GPSO implements Algorithm {
             Utility.cur_generation++;
 
             recordVariables(sub1);
+            recordScatter(sub1);
             //writeFile();
         }
         /* Input best particle for Output */
         Utility.stack_hist_best_fit[run] = sub1.getLbest_fitness();
         //Utility.printFinalBest(run, sub1);
         writeParameterFiles();
+        writeScatterFiles();
     }
 
     /*
@@ -112,10 +117,10 @@ public class GPSO implements Algorithm {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        recordGeneration.clear();
-        recordEvalTimes.clear();
-        recordBestFitness.clear();
-        recordInertiaWeight.clear();
+//        recordGeneration.clear();
+//        recordEvalTimes.clear();
+//        recordBestFitness.clear();
+//        recordInertiaWeight.clear();
     }
 
     private void recordVariables(SubSwarm swarm){
@@ -123,5 +128,73 @@ public class GPSO implements Algorithm {
         recordEvalTimes.add(Utility.cur_func_eval);
         recordInertiaWeight.add(currentIW);
         recordBestFitness.add(swarm.getLbest_fitness());
+    }
+
+    private void makeScatterFiles(int run){
+        /* Make Folder */
+        String className = new Object(){}.getClass().getEnclosingClass().getName();
+        folderName = "/Users/toshiki/Output/" + Utility.date + "/" + className +
+                "/" + ProblemUtil.getProb_obj() + "/RUN" + run;
+        File newfile = new File(folderName);
+        newfile.mkdirs();
+        /* Make csv file */
+        try {
+            //Make csv file
+            FileWriter fw = new FileWriter(folderName + "/scatter.csv", false); //上書きモード
+            //Write header
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            pw.print("Generation");
+            pw.print(",");
+            pw.print("eval_times");
+            pw.print(",");
+            pw.print("individual_index");
+            pw.print(",");
+            for (int dim = 0; dim < Utility.getDIMENSION(); dim++) {
+                pw.print("Dim" + dim);
+                pw.print(",");
+            }
+            pw.println();
+            //Close file
+            pw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void recordScatter(SubSwarm swarm){
+        recordPosition.add(swarm);
+    }
+
+    private void writeScatterFiles(){
+        try {
+            FileWriter fw = new FileWriter(folderName + "/scatter.csv", true); //追記モード
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            int size = recordPosition.size();
+            for (int ind =0; ind < size; ind++) {
+                int par_ind = 0;
+                for (Particle par : recordPosition.get(ind).particles) {
+                    pw.print(recordGeneration.get(ind));
+                    pw.print(",");
+                    pw.print(recordEvalTimes.get(ind));
+                    pw.print(",");
+                    pw.print(par_ind++);
+                    pw.print(",");
+                    for (int dim = 0; dim < Utility.getDIMENSION(); dim++){
+                        pw.print(par.positon[dim]);
+                        pw.print(",");
+                    }
+                    pw.println();
+                }
+            }
+            //Close file
+            pw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        recordPosition.clear();
+        recordGeneration.clear();
+        recordEvalTimes.clear();
+        recordBestFitness.clear();
+        recordInertiaWeight.clear();
     }
 }
